4d8836d4dc44aac9afce45afa2e145f916bd7ecf
Romanov Caesar <88546123+RomanovCaesar@users.noreply.github.com>
2026-04-08 17:16:06 +0800

    Update install_ss2022.sh

--- install_ss2022.sh
+++ install_ss2022.sh
@@ -659,9 +659,9 @@ main_menu() {
         echo -e "${cyan} Xray Shadowsocks 2022 管理脚本${none}"
         echo "---------------------------------------------"
         echo -e "${red} 如果您是在uzumaru购买的产品，并且该产品${none}"
-        echo -e "${red} 是用IDC入口IP或者是DDNS域名连接的${none}"
-        echo -e "${red} 请先使用功能9，填入uzumaru网站面板上显示的连接IP或DDNS域名${none}"
-        echo -e "${red} 避免创建节点后因使用错误连接地址而不通。${none}"
+        echo -e "${red} 是用IDC入口IP或者是DDNS域名连接的，请${none}"
+        echo -e "${red} 先使用功能9，填入uzumaru网站面板上显示的连接IP或DDNS域名${none}"
+        echo -e "${red} 避免创建节点后因使用的连接地址错误而不通。${none}"
         echo "---------------------------------------------"
         check_xray_status
         echo -e "${xray_status_info}"
